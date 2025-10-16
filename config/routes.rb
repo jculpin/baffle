931ed3e873b6198d676505e206d3cf319acacaab
John Culpin <john@johnculpin.co.uk>
2025-10-16 17:45:22 +0100

    Fix linting issues

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  #get "/products", to: "products#index"
+  # get "/products", to: "products#index"
 
-  #get "/products/new", to: "products#new"
-  #post "/products", to "products#create"
+  # get "/products/new", to: "products#new"
+  # post "/products", to "products#create"
 
-  #get "/products/:id", to "products#show"
+  # get "/products/:id", to "products#show"
   
-  #get "products/:id/edit", to: "products#edit"
-  #patch "/products/:id", to: "products#update"
-  #put "/products/:id",, to: "products#update"
+  # get "products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id",, to: "products#update"
 
-  #delete "/products/:id", to: "products#destroy"
+  # delete "/products/:id", to: "products#destroy"
 
   root "products#index"
   resources :products
--- setuplog.md
+++ setuplog.md
@@ -11,4 +11,12 @@ https://guides.rubyonrails.org/install_ruby_on_rails.html#install-ruby-on-ubuntu
 - (updated gems using gem update --system 3.7.2)
 - Created new Rails app (ruby new baffle)
 
-- Add a database table (bin/rails generate model Product name::string)
+- Add a database table (bin/rails generate model Product name:string)
+- Run the migration (bin/rails db:migrate)
+
+- Run console (bin/rails console)
+- Check columns in new product model (Product.column_names)
+- Add a new record using product = Product.new(name: "T-Shirt")
+- Save the new record using product.save
+- 
+
